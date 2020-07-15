41ef6c2dfea52e923f04626f0df97c4d68ef0ddb
Nick Gao <sinhbv@gmail.com>
2020-07-15 16:19:26 +0800

    hide QQ login

--- site/pages/user/signin.vue
+++ site/pages/user/signin.vue
@@ -12,10 +12,10 @@
               <div class="control has-icons-left">
                 <input
                   v-model="username"
-                  @keyup.enter="submitLogin"
+                  placeholder="请输入用户名或邮箱"
                   class="input is-success"
                   type="text"
-                  placeholder="请输入用户名或邮箱"
+                  @keyup.enter="submitLogin"
                 />
                 <span class="icon is-small is-left"
                   ><i class="iconfont icon-username"
@@ -28,10 +28,10 @@
               <div class="control has-icons-left">
                 <input
                   v-model="password"
-                  @keyup.enter="submitLogin"
                   class="input"
                   type="password"
                   placeholder="请输入密码"
+                  @keyup.enter="submitLogin"
                 />
                 <span class="icon is-small is-left"
                   ><i class="iconfont icon-password"
@@ -46,11 +46,11 @@
                   <div class="field">
                     <input
                       v-model="captchaCode"
-                      @keyup.enter="submitLogin"
                       class="input"
                       type="text"
                       placeholder="验证码"
                       style="max-width: 150px; margin-right: 20px;"
+                      @keyup.enter="submitLogin"
                     />
                     <span class="icon is-small is-left"
                       ><i class="iconfont icon-captcha"
@@ -67,11 +67,11 @@
 
             <div class="field">
               <div class="control">
-                <button @click="submitLogin" class="button is-success">
+                <button class="button is-success" @click="submitLogin">
                   登录
                 </button>
                 <github-login :ref-url="ref" />
-                <qq-login :ref-url="ref" />
+                <!-- <qq-login :ref-url="ref" /> -->
                 <nuxt-link class="button is-text" to="/user/signup">
                   没有账号？点击这里去注册&gt;&gt;
                 </nuxt-link>
@@ -87,11 +87,11 @@
 <script>
 import utils from '~/common/utils'
 import GithubLogin from '~/components/GithubLogin'
-import QqLogin from '~/components/QqLogin'
+// import QqLogin from '~/components/QqLogin'
 export default {
   components: {
-    GithubLogin,
-    QqLogin
+    GithubLogin
+    // QqLogin
   },
   asyncData({ params, query }) {
     return {
