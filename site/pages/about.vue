f7b5c2db844728e31e3487e2aa3f160275b02bad
Nick Gao <sinhbv@gmail.com>
2020-07-30 19:39:48 +0800

    update about page

--- site/pages/about.vue
+++ site/pages/about.vue
@@ -8,26 +8,28 @@
         <div class="widget-content content">
           <h2>简介</h2>
           <p>
-            BBS-GO&nbsp;是一款基于Go语言研发的精美小巧的开源社区论坛系统。初期该项目仅用过学习和交流，开源之后越来越多的小伙伴儿开始喜欢和关注他，这也是我长期升级和维护的动力。
+            BBS-GO&nbsp;是一款基于Go语言研发的精美小巧的开源社区论坛系统。
           </p>
 
           <p>
-            为方便大家学习和交流我们在2019年8月使用&nbsp;BBS-GO&nbsp;搭建了&nbsp;<a
-              href="https://mlog.club"
-              target="_blank"
-              >码农俱乐部</a
-            >，欢迎大家前往交流。
+            Telework在BBS-Go的基础上，对功能进行了一部分修改，形成了现在的社区系统。
           </p>
 
           <h2>与我联系</h2>
           <p>
-            如有任何想法、意见、问题可前往&nbsp;<a
+            如有需要可以在
+            <a href="https://github.com/telework-club/forum/issues">Github</a>
+            上发issue，也可以直接发邮件
+            <a href="mailto://core.telework.club?subject=反馈">联系我们</a>
+          </p>
+          <p>
+            联系原项目作者可前往&nbsp;<a
               href="https://mlog.club"
               target="_blank"
               >码农俱乐部</a
             >&nbsp;交流。
           </p>
-          <h2>项目贡献者</h2>
+          <h2>原项目贡献者</h2>
           <p>
             <a
               href="https://github.com/mlogclub/bbs-go/graphs/contributors"
